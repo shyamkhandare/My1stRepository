08ce2828a66514920c4f22c6858ea5908000ec00
kishorwadkar <kishorwadkar1717@gmail.com>
2022-09-07 23:56:09 +0530

    Example1
    Example1 testcase Created give aproval for it.

---
+++ MyRepository/src/qa/com/testcase/Example1.java
@@ -0,0 +1,12 @@
+package qa.com.testcase;
+
+public class Example1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hii Sham");
+		System.out.println("Welcom to git hub");
+		
+	}
+
+}
